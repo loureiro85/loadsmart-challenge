875a0be0c249301192a7098cceede36ec691ea29
Guilherme Loureiro <loureiro85@gmail.com>
2022-06-27 02:55:52 +0000

    refactor: Rename stg to dim_us_cities

--- models/staging/internal/stg_join_lat_long_from.sql
+++ models/staging/internal/stg_join_lat_long_from.sql
@@ -10,5 +10,5 @@ lat_to,
 long_to
 
 from {{ ref('stg_join_lat_long_to') }} 
-left join {{ ref('stg_us_cities') }} 
-on {{ ref('stg_us_cities') }}.city_state={{ ref('stg_join_lat_long_to') }}.lane_from
+left join {{ ref('dim_us_cities') }} 
+on {{ ref('dim_us_cities') }}.city_state={{ ref('stg_join_lat_long_to') }}.lane_from
--- models/staging/internal/stg_join_lat_long_to.sql
+++ models/staging/internal/stg_join_lat_long_to.sql
@@ -5,5 +5,5 @@ lane_from,
 latitude as lat_to,
 longitude as long_to
 from {{ ref('stg_city_state') }} 
-left join {{ ref('stg_us_cities') }} 
-on {{ ref('stg_us_cities') }}.city_state={{ ref('stg_city_state') }}.lane_to
+left join {{ ref('dim_us_cities') }} 
+on {{ ref('dim_us_cities') }}.city_state={{ ref('stg_city_state') }}.lane_to
