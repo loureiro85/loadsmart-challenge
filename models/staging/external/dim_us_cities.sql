ea7e582e3b7e716814fdfef91cce90048d1c330b
Guilherme Loureiro <loureiro85@gmail.com>
2022-06-28 16:03:51 +0000

    feat: Implement tests and docs.

--- models/staging/external/dim_us_cities.sql
+++ models/staging/external/dim_us_cities.sql
@@ -3,3 +3,6 @@ select *,
 city || ',' ||state_code as city_state
 
  from {{ source('external', 'us_cities') }}
+
+where city=county -- required to drop duplicated cities 
+and city_state is not null
