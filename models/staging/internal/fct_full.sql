45bff5eed167bd417ee8263a7c31b431a82cb069
Guilherme Loureiro <loureiro85@gmail.com>
2022-06-28 18:53:13 +0000

    feat: Implement analytics tables delivery and pickup grouped by month and state.

--- models/staging/internal/fct_full.sql
+++ models/staging/internal/fct_full.sql
@@ -1,19 +1,20 @@
 select 
-    year_month as pickup_month,
-    weekday_name as pickup_weekday,
-    -- pickup_state,
-    -- delivery_state,
+    year_month_p as pickup_month,
+    weekday_name_p as pickup_weekday,
+    year_month_d as delivery_month,
+    weekday_name_d as delivery_weekday,
     round((pnl/book_price) * 100, 1) as pnl_percent,
     *
 
 from {{ ref('fct_pre_processed') }}
 
 left join {{ ref('dim_lane') }}
-on {{ ref('fct_pre_processed') }}.lane={{ ref('dim_lane') }}.lane_id
+on lane={{ ref('dim_lane') }}.lane_id
 
-left join {{ ref('dim_calendar') }}
-on date({{ ref('fct_pre_processed') }}.pickup_date) = {{ ref('dim_calendar') }}.date
+left join (select date as date_p, year_month as year_month_p, weekday_name as weekday_name_p from {{ ref('dim_calendar') }})
+on date(pickup_date) = date_p
+
+left join (select date as date_d, year_month as year_month_d, weekday_name as weekday_name_d from {{ ref('dim_calendar') }})
+on date(delivery_date) = date_d
 
 where book_price > 0
-
-order by pickup_month
